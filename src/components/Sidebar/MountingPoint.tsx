c66587cc2abaa8ab98ba0713fe56b3a8f755a808
Paul Tindall <paul@wellstandhealth.com>
2023-04-07 09:21:22 -0500

    Fix audit issues

--- src/components/Sidebar/MountingPoint.tsx
+++ src/components/Sidebar/MountingPoint.tsx
@@ -1,4 +1,4 @@
-import sideBarDefaultLogo from "@assets/images/sidebar-default-logo.png";
+import sideBarDefaultLogo from "@assets/images/sidebar-default-logo.svg";
 import { Avatar, Box, Text } from "@saleor/macaw-ui/next";
 import React from "react";
 
@@ -6,7 +6,7 @@ export const MountingPoint = () => (
   <Box display="flex" gap={6} paddingX={7} paddingY={8} alignItems="center">
     <Avatar.Store src={sideBarDefaultLogo} scheme="decorative2" size="small" />
     <Text variant="bodyStrong" size="small">
-      Saleor Dashboard
+      WellStand Health Dashboard
     </Text>
   </Box>
 );
